108d59c2f631b709a510833ced1488a71974723a
bamin0422 <bamin0422@gmail.com>
2021-07-31 21:45:42 +0900

    Add permission of camera

--- app/src/main/java/kobot/board/onego/activity/CameraActivity.kt
+++ app/src/main/java/kobot/board/onego/activity/CameraActivity.kt
@@ -1,38 +1,95 @@
 package kobot.board.onego.activity
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.ImageFormat
 import android.graphics.drawable.Drawable
+import android.hardware.Sensor
+import android.hardware.SensorManager
+import android.hardware.camera2.*
+import android.media.ExifInterface
+import android.media.ImageReader
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.DisplayMetrics
+import android.util.Log
+import android.util.SparseIntArray
+import android.view.SurfaceHolder
 import android.view.SurfaceView
+import android.view.WindowManager
+import android.widget.CheckBox
+import android.widget.FrameLayout
 import android.widget.ImageButton
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kobot.board.onego.R
 
 class CameraActivity : AppCompatActivity() {
 
-    lateinit var flashBtn : ImageButton
+    lateinit var mSurfaceViewHolder : SurfaceHolder
+    lateinit var mImageReader: ImageReader
+    lateinit var mCameraDevice : CameraDevice
+    lateinit var mPreviewBuilder : CaptureRequest.Builder
+    lateinit var mSession : CameraCaptureSession
+
+    var mHandler : Handler? = null
+
+    var mHeight : Int = 0
+    var mWidth : Int = 0
+
+    var mCameraId = CAMERA_BACK
+
+    companion object{
+        const val CAMERA_BACK = "0"
+        const val CAMERA_FRONT = "1"
+
+        private val ORIENTATIONS = SparseIntArray()
+
+        init {
+            ORIENTATIONS.append(ExifInterface.ORIENTATION_NORMAL, 0)
+            ORIENTATIONS.append(ExifInterface.ORIENTATION_ROTATE_90, 90)
+            ORIENTATIONS.append(ExifInterface.ORIENTATION_ROTATE_180, 180)
+            ORIENTATIONS.append(ExifInterface.ORIENTATION_ROTATE_270, 270)
+        }
+    }
+
+    lateinit var spinBtn : ImageButton
     lateinit var closeBtn : ImageButton
     lateinit var shutterBtn : ImageButton
     lateinit var cameraPreview : SurfaceView
     var flashBtnSwitch = 0
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
 
-        flashBtn = findViewById(R.id.flashBtn)
+        spinBtn = findViewById(R.id.spinBtn)
         closeBtn = findViewById(R.id.closeBtn)
         cameraPreview = findViewById(R.id.cameraPreview)
         shutterBtn = findViewById(R.id.shutterBtn)
 
-        flashBtn.setOnClickListener {
-            if(flashBtnSwitch == 0){
-                shutterBtn.setBackgroundResource(R.drawable.flash)
-                flashBtnSwitch = 1
-            }else{
-                shutterBtn.setBackgroundResource(R.drawable.non_flash)
-                flashBtnSwitch = 0
-            }
+        // hide status bar
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        // turn on screen
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+            WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+        )
+
+        spinBtn.setOnClickListener {
+
         }
         closeBtn.setOnClickListener {
             finish()
@@ -42,4 +99,5 @@ class CameraActivity : AppCompatActivity() {
             startActivity(Intent(this, ImageActivity::class.java))
         }
     }
+
 }
--- app/src/main/java/kobot/board/onego/activity/MainActivity.kt
+++ app/src/main/java/kobot/board/onego/activity/MainActivity.kt
@@ -1,6 +1,7 @@
 package kobot.board.onego.activity
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,28 +9,27 @@ import android.util.DisplayMetrics
 import android.widget.Button
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
 import kobot.board.onego.R
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
     lateinit var cameraBtn : Button
     lateinit var galleryBtn : Button
+    lateinit var fileRecyclerView : RecyclerView
 
     @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var display = windowManager.defaultDisplay
-        var outMetrics = DisplayMetrics()
-        display.getRealMetrics(outMetrics)
 
-        var density = resources.displayMetrics.density
 
         cameraBtn = findViewById(R.id.cameraButton)
         galleryBtn = findViewById(R.id.galleryButton)
-
-        cameraBtn.height = (outMetrics.heightPixels / density).toInt()
-        galleryBtn.height = cameraBtn.height
+        fileRecyclerView = findViewById(R.id.fileListView)
 
         cameraBtn.setOnClickListener {
             startActivity(Intent(this, CameraActivity::class.java))
@@ -40,4 +40,5 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "갤러리 버튼이 클릭되었습니다.", Toast.LENGTH_SHORT).show()
         }
     }
+
 }
